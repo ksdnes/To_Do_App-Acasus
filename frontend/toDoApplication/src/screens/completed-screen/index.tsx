854143c2ff6e8c06150a0446efcc707081d1543f
ksdnes <108872337+ksdnes@users.noreply.github.com>
2024-07-03 01:11:36 +0200

    feat : implement the completed screen (frontend)

--- frontend/toDoApplication/src/screens/completed-screen/index.tsx
+++ frontend/toDoApplication/src/screens/completed-screen/index.tsx
@@ -1,15 +1,53 @@
+import Loader from "@/components/shared/loader";
+import SafeAreaWrapper from "@/components/shared/safe-area-wrapper";
+import Track from "@/components/tracks/track";
+import { fetcher } from "@/services/config";
+import useUserGlobalStore from "@/store/useUserGlobalStore";
+import { ITrack } from "@/types";
+import { getGreeting } from "@/utils/helpers";
 import { Box, Text } from "@/utils/theme";
 import React from "react";
-import { Button } from "react-native";
+import { FlatList } from "react-native";
+import { ZoomInEasyDown } from "react-native-reanimated";
+import useSWR from "swr";
 import { useNavigation } from "@react-navigation/native";
-import { AuthScreenNavigationType } from "@/navigation/types";
-import SafeAreaWrapper from "@/components/shared/safe-area-wrapper";
-
+import { Pressable } from "react-native";
 const CompletedScreen = () => {
+  const navigation = useNavigation<AuthScreenNavigationType<"Me">>();
+  const [triggerLogout, setTriggerLogout] = React.useState(false);
+
+  const { user, updateUser } = useUserGlobalStore();
+  const handleLogout = async () => {
+    try {
+      updateUser(null);
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
+  const {
+    data: tracks,
+    isLoading,
+    mutate: mutateTracks,
+  } = useSWR<ITrack[]>(`/running-tracks/user/${user.id}/completed`, fetcher);
+
+  if (isLoading || !tracks) {
+    return <Loader />;
+  }
+
   return (
     <SafeAreaWrapper>
-      <Box>
-        <Text>Completed Screen</Text>
+      <Box flex={1} mx="4">
+        <Box height={26} />
+        <Box height={26} />
+        <FlatList
+          data={tracks}
+          renderItem={({ item }) => (
+            <Track mutateTracks={mutateTracks} track={item} user={user.name} />
+          )}
+          ItemSeparatorComponent={() => <Box height={14} />}
+          showsVerticalScrollIndicator={false}
+          keyExtractor={(item) => item._id}
+        />
       </Box>
     </SafeAreaWrapper>
   );
