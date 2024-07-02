978100684f1e708f39ba7ec40265ecd6c024759f
ksdnes <108872337+ksdnes@users.noreply.github.com>
2024-07-02 13:31:10 +0200

    feat : implement the logout - temporary

--- frontend/toDoApplication/src/navigation/index.tsx
+++ frontend/toDoApplication/src/navigation/index.tsx
@@ -1,14 +1,16 @@
 import { NavigationContainer } from "@react-navigation/native";
 import AuthStackNavigator from "./auth-stack-navigator";
 import AppStackNavigator from "./app-stack-navigator";
-import useUserGlobalSzore from "@/store/useUserGlobalStore";
+import useUserGlobalStore from "@/store/useUserGlobalStore";
 import React from "react";
 const Navigation = () => {
-  const { user, updateUser } = useUserGlobalSzore();
-
+  const { user, updateUser } = useUserGlobalStore();
+  React.useEffect(() => {
+    console.log("user" + user);
+  }, [user]);
   return (
     <NavigationContainer>
-      {user ? <AppStackNavigator /> : <AuthStackNavigator />}
+      {!user ? <AuthStackNavigator /> : <AppStackNavigator />}
     </NavigationContainer>
   );
 };
--- frontend/toDoApplication/src/screens/mee-screen/index.tsx
+++ frontend/toDoApplication/src/screens/mee-screen/index.tsx
@@ -1,16 +1,35 @@
+import React, { useState, useEffect } from "react";
 import { Box, Text } from "@/utils/theme";
-import React from "react";
-import { Button } from "react-native";
+import { Pressable } from "react-native";
+import SafeAreaWrapper from "@/components/shared/safe-area-wrapper";
 import { useNavigation } from "@react-navigation/native";
 import { AuthScreenNavigationType } from "@/navigation/types";
-import SafeAreaWrapper from "@/components/shared/safe-area-wrapper";
+import { useApi, AXIOS_METHOD } from "@/services/useApi";
+import * as SecureStore from "expo-secure-store";
+import axiosInstance from "@/services/config";
+import { logoutUser } from "@/services/api";
+import useUserGlobalStore from "@/store/useUserGlobalStore";
 
 const MeeScreen = () => {
+  const navigation = useNavigation<AuthScreenNavigationType<"Me">>();
+  const [triggerLogout, setTriggerLogout] = useState(false);
+  const { user, updateUser } = useUserGlobalStore();
+
+  const handleLogout = async () => {
+    try {
+      updateUser(null);
+    } catch (error) {
+      console.error("Logout failed", error);
+    }
+  };
+
   return (
     <SafeAreaWrapper>
-      <Box>
-        <Text>Me</Text>
-      </Box>
+      <Pressable onPress={handleLogout}>
+        <Text color="primary" textAlign="right" ml="1">
+          Logout
+        </Text>
+      </Pressable>
     </SafeAreaWrapper>
   );
 };
